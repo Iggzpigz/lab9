6bfc2fc1720edfeccdfd1a8799a9d7812a0c8f19
Jacob <jaclucb2.0@gmail.com>
2025-05-07 15:53:18 +1000

    this work?

--- settings.php
+++ settings.php
@@ -6,9 +6,9 @@
     <body>
         <?php 
         $host = "localhost";         // because XAMPP runs the server locally
-        $user = "root";          // default username for XAMPP's MySQL
-        $pwd = "";              // default password is empty in XAMPP
-        $sql_db = "exhibition_db";  // replace with the actual name of your database
+        $useranme = "root";          // default username for XAMPP's MySQL
+        $password = "";              // default password is empty in XAMPP
+        $database = "exhibition_db";  // replace with the actual name of your database
 
         $conn = mysqli_connect($host, $username, $password, $database);
 
