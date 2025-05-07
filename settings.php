3ab6fd15b70fd9398ca3325983fae91bbb1eaaae
Jacob <jaclucb2.0@gmail.com>
2025-05-07 15:15:04 +1000

    asdf

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@
         $host = "localhost";         // because XAMPP runs the server locally
         $user = "root";          // default username for XAMPP's MySQL
         $pwd = "";              // default password is empty in XAMPP
-        $sql_db = "your_db_name";  // replace with the actual name of your database
+        $sql_db = "exhibition_db";  // replace with the actual name of your database
         ?>
     </body>
 </html>
